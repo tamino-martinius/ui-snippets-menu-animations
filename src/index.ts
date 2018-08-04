3497dbf89a15224af90eb62a51475146059cf5b3
Tamino Martinius <dev@zaku.eu>
2018-08-04 10:27:02 +0200

    Add class active

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log('Hello World');
+// window.addEventListener('click', () => {
+Array.from(document.getElementsByClassName('menu')).map(menu => {
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active');
+  } else {
+    menu.classList.add('active');
+  }
+});
+// });
