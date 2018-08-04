b6674ac75dd3fcd2e182bbd82bb39a19ce2bdb34
Tamino Martinius <dev@zaku.eu>
2018-08-04 18:08:50 +0200

    Add debug lines

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-Array.from(document.getElementsByTagName('path')).map(path =>
-  console.log(path.getTotalLength())
-);
+Array.from(document.getElementsByTagName('path')).map(path => {
+  console.log(path.getTotalLength());
+  const debugPath: SVGPathElement = <any>path.cloneNode();
+  debugPath.setAttribute('class', '');
+  debugPath.classList.add('line--debug');
+  if (path.parentNode) path.parentNode.insertBefore(debugPath.cloneNode(), path);
+});
 // window.addEventListener('click', () => {
 Array.from(document.getElementsByClassName('menu')).map(menu => {
   if (menu.classList.contains('active')) {
