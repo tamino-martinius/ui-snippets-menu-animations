334d783769fd0e884bf49a12d1acb6a1b343243b
Tamino Martinius <dev@zaku.eu>
2018-08-06 06:43:58 +0200

    Remove script

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,3 @@ Array.from(document.getElementsByTagName('path')).map(path => {
   debugPath.classList.add('line--debug');
   if (path.parentNode) path.parentNode.insertBefore(debugPath.cloneNode(), path);
 });
-window.addEventListener('click', () => {
-  Array.from(document.getElementsByClassName('menu')).map(menu => {
-    if (menu.classList.contains('active')) {
-      menu.classList.remove('active');
-    } else {
-      menu.classList.add('active');
-    }
-  });
-});
