5a2398bfb08f495dc86ce1161ffc2b4b747f4571
Tamino Martinius <dev@zaku.eu>
2018-08-06 18:43:53 +0200

    Add interactions and autoplay

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 Array.from(document.getElementsByTagName('path')).map(path => {
   console.log(path.getTotalLength());
-  const debugPath: SVGPathElement = <any>path.cloneNode();
+  const debugPath: HTMLElement = <any>path.cloneNode();
   debugPath.classList.add('line--debug');
   if (path.parentNode) path.parentNode.insertBefore(debugPath.cloneNode(), path);
 });
+const debugCheckbox: HTMLInputElement = <any>document.getElementById('debug');
+debugCheckbox.addEventListener('change', () => {
+  if (debugCheckbox.parentElement) {
+    if (debugCheckbox.checked) {
+      debugCheckbox.parentElement.classList.add('active');
+    } else {
+      debugCheckbox.parentElement.classList.remove('active');
+    }
+  }
+});
+let currentActive = 0;
+const checkboxes: HTMLInputElement[] = <any>document.querySelectorAll('.grid input');
+const autoShow = setInterval(() => {
+  checkboxes[currentActive % 4].checked = !checkboxes[currentActive % 4].checked;
+  if (!checkboxes[currentActive % 4].checked) currentActive += 1;
+}, 1000);
+const grid = document.querySelector('.grid');
+if (grid) {
+  grid.addEventListener('click', () => {
+    clearInterval(autoShow);
+  });
+}
